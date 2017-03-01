94f3434d5dcf83bb04ed34fb0885e5bf8e1a27c2
Tori Hedden <tori.hedden@gmail.com>
2017-03-01 18:47:36 -0500

    exercise 1

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
 // Exercises from last night
+//
+// The below function does not work. The variable twoPlusTwo gets set to undefined. Refactor the function to make it work.
+
+function addNumbers(numberA, numberB) {
+  var result = numberA + numberB;
+  return result;
+}
+
+var twoPlusTwo = addNumbers(2, 2);
+
+console.log(twoPlusTwo); // 4 is the expected output from this function with these given inputs
