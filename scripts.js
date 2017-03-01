04009c4b26e00dcad45f1a24e32ecbf269883483
Tori Hedden <tori.hedden@gmail.com>
2017-03-01 18:10:50 -0500

    remove old demo code

--- scripts.js
+++ scripts.js
@@ -1,16 +1,1 @@
-// objects in JavaScript
-
-// var dogName = "Bucky";
-// var dogColor = "black";
-// var dogAge = 14;
-// var dogIsBlind = true;
-
-var dog = {
-  name      : "Bucky",
-  color     : "black",
-  age       : 14,
-  isBlind   : true,
-  ownerName : "Bernice"
-}
-
-console.log(dog.ownerName);
+// Exercises from last night
