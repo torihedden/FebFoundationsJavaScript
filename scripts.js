9afcbfa9f6e10e28ebf11300aa492521371097bc
Tori Hedden <tori.hedden@gmail.com>
2017-03-01 19:35:56 -0500

    check if vowel exercise

--- scripts.js
+++ scripts.js
@@ -1,14 +1,13 @@
-// Write a function called longest that returns the longest of two input strings or arrays.
+// Write a function called isVowel that takes a character (i.e. a string of length 1) and returns true if it is a vowel, uppercase or lowercase. The function should return false if the character is not a vowel.
 
 
-function longer(a, b) {
-  if (a.length === b.length) {
-    return "they are the same length";
-  } else if (a.length > b.length) {
-    return a;
-  } else if (b.length > a.length) {
-    return b;
+function isVowel (character){
+  var vowel = ["A", "E", "I", "O", "U"];
+  if (vowel.indexOf(character.toUpperCase()) > -1) {
+    return true;
+  } else {
+    return false;
   }
 }
 
-console.log(longer("string", "abcdefghijklmnop"));
+console.log(isVowel("A"));
